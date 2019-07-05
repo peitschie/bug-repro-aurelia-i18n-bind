ea67979eecb4c0224c32ffc1d1454db295519114
Philip Peitsch <philip.peitsch@gmail.com>
2019-07-05 14:20:43 +1000

    Demonstrate aurelia-i18n binding issue when model changes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
 export class App {
   public message: string = 'Hello World!';
+  public model?: any;
+
+  constructor() {
+    this.loadModelA();
+  }
+
+  loadModelA() {
+    this.model = {
+      line1: "model-a.line1",
+      line2: "model-a.line2"
+    }
+  }
+
+  loadModelB() {
+    this.model = {
+      line1: "model-b.line1",
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import {Aurelia} from 'aurelia-framework'
-import environment from './environment';
-import {PLATFORM} from 'aurelia-pal';
-import { Backend, TCustomAttribute } from 'aurelia-i18n';
+import { Aurelia } from "aurelia-framework";
+import environment from "./environment";
+import { PLATFORM } from "aurelia-pal";
+import { Backend, TCustomAttribute } from "aurelia-i18n";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'))
-    .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance: any) => {
-      let aliases = ['t', 'i18n'];
+    .plugin(PLATFORM.moduleName("aurelia-i18n"), (instance: any) => {
+      let aliases = ["t"];
       TCustomAttribute.configureAliases(aliases);
 
-      instance.i18next
-        .use(Backend.with(aurelia.loader))
+      instance.i18next.use(Backend.with(aurelia.loader));
 
       return instance.setup({
         resources: {
-          "en": {
+          en: {
             translation: {
-            "message": "testing parameter binding from {{source}}"
+              "model-a": {
+                line1: "Model A line 1 translation",
+                line2: "Model A line 2 translation"
+              },
+              "model-b": {
+                line1: "Model B line 1 translation"
+              }
             }
           }
         },
@@ -26,12 +30,13 @@ export function configure(aurelia: Aurelia) {
         lng: "en",
         debug: true
       });
+    });
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+  aurelia.use.developmentLogging(environment.debug ? "debug" : "warn");
 
   if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    aurelia.use.plugin(PLATFORM.moduleName("aurelia-testing"));
   }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName("app")));
 }
